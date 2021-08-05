46a656442ceeb9452e3b3cfd54dd46bcc908c70b
saikatXshrey <saikatxshrey@gmail.com>
2021-08-06 02:52:24 +0530

    files uploded fr pocket radio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Radio from './Radio';
+import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shrey's Pocket Radio App</h1>
+      <h2>Pick a genre, choose a station, start listening</h2>
+      <Radio />
     </div>
-  );
+  )
 }
 
 export default App;
